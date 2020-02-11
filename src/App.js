e9765843617b31f23a1e707eb71d35c0987640ea
Vilva <alberto.villar@qatium.com>
2020-02-11 16:00:55 +0100

    Mimic redux actions with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useContext, createContext } from 'react';
 import './App.css';
 
-function App() {
+const initialState = {
+  count: 0,
+}
+
+const counterReducer = (state, action) => {
+  return {
+    ...state,
+    count: state.count + 1,
+  }
+}
+
+const Counter = ({incrementCount, count}) => {
+
+  return(
+    <div>
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  )
+}
+
+const OutsideIncrement = ({incrementCount}) => {
+  return(
+    <button onClick={incrementCount}>Increment from outside</button>
+  )
+}
+
+const App = () => {
+
+  const [state, dispatch] = useReducer(counterReducer, initialState)
+
+  const incrementCount = () => {
+    dispatch({
+      type: 'INCREMENT'
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter incrementCount={incrementCount} count={state.count}/>
+      <OutsideIncrement incrementCount={incrementCount}/>
     </div>
   );
 }
 
-export default App;
+export default App
