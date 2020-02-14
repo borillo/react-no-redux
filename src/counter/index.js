d0b02cd76e29dff880796bab3bf963300424cbf1
Ricardo Borillo <borillo@gmail.com>
2020-02-14 23:21:45 +0100

    Merge files. Prefer hooks approach

--- src/counter/index.js
+++ src/counter/index.js
@@ -1,18 +1,63 @@
-import React from "react";
-import { Container } from "./container";
-import { incrementCount } from "./actions";
-import { useCounterContext } from "./context";
+import React, { useReducer } from "react";
+
+import { CounterService } from "./services/CounterService";
+import { Counter } from "./components/Counter";
+import { Loader } from "./components/Loader";
+
+export const INCREMENT_START = "INCREMENT_START";
+export const INCREMENT_SUCCESS = "INCREMENT_SUCCESS";
+
+const initialState = {
+  count: 0,
+  isCalculating: false
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT_START:
+      return {
+        ...state,
+        isCalculating: true
+      };
+    case INCREMENT_SUCCESS:
+      return {
+        ...state,
+        count: state.count + 1,
+        isCalculating: false
+      };
+    default:
+      return {
+        ...state
+      };
+  }
+};
+
+function useCounter() {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+
+  async function onIncrement() {
+    dispatch({ type: INCREMENT_START });
+
+    await CounterService.costlyIncrement();
+
+    dispatch({ type: INCREMENT_SUCCESS });
+  }
+
+  return {
+    ...state,
+    onIncrement
+  };
+}
 
 const ConnectedCounter = () => {
-  const [state, dispatch] = useCounterContext();
+  const { isCalculating, count, onIncrement } = useCounter();
 
-  const mapAllToProps = {
-    onIncrement: () => incrementCount(dispatch),
-    count: state.count,
-    isCalculating: state.isCalculating
-  };
-
-  return <Container {...mapAllToProps} />;
+  return (
+    <>
+      {isCalculating ? <Loader /> : <Counter count={count} />}
+      <button onClick={onIncrement}>Increment</button>
+    </>
+  );
 };
 
 export { ConnectedCounter };
